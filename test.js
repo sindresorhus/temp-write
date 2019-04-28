1b87cb1581ce98f98c235cf922a5064289191a7b
Dimitri Benin <BendingBender@users.noreply.github.com>
2019-04-28 14:00:32 +0000

    Require Node.js 8, add TypeScript definition (#12)

--- test.js
+++ test.js
@@ -2,23 +2,23 @@ import fs from 'fs';
 import path from 'path';
 import stream from 'stream';
 import test from 'ava';
-import m from '.';
+import tempWrite from '.';
 
 test('tempWrite(string)', async t => {
-	const filepath = await m('unicorn', 'test.png');
-	t.is(fs.readFileSync(filepath, 'utf8'), 'unicorn');
-	t.is(path.basename(filepath), 'test.png');
+	const filePath = await tempWrite('unicorn', 'test.png');
+	t.is(fs.readFileSync(filePath, 'utf8'), 'unicorn');
+	t.is(path.basename(filePath), 'test.png');
 });
 
 test('tempWrite(buffer)', async t => {
-	const filepath = await m(Buffer.from('unicorn'), 'test.png');
-	t.is(fs.readFileSync(filepath, 'utf8'), 'unicorn');
+	const filePath = await tempWrite(Buffer.from('unicorn'), 'test.png');
+	t.is(fs.readFileSync(filePath, 'utf8'), 'unicorn');
 });
 
 test('tempWrite(buffer, path)', async t => {
-	const filepath = await m(Buffer.from('unicorn'), 'foo/bar/test.png');
-	t.is(fs.readFileSync(filepath, 'utf8'), 'unicorn');
-	t.regex(filepath, /foo\/bar\/test\.png$/);
+	const filePath = await tempWrite(Buffer.from('unicorn'), 'foo/bar/test.png');
+	t.is(fs.readFileSync(filePath, 'utf8'), 'unicorn');
+	t.regex(filePath, /foo\/bar\/test\.png$/);
 });
 
 test('tempWrite(stream)', async t => {
@@ -27,10 +27,10 @@ test('tempWrite(stream)', async t => {
 	});
 	readable.push('unicorn');
 	readable.push(null);
-	const filepath = await m(readable, 'test.png');
-	t.is(fs.readFileSync(filepath, 'utf8'), 'unicorn');
+	const filePath = await tempWrite(readable, 'test.png');
+	t.is(fs.readFileSync(filePath, 'utf8'), 'unicorn');
 });
 
 test('tempWrite.sync()', t => {
-	t.is(fs.readFileSync(m.sync('unicorn'), 'utf8'), 'unicorn');
+	t.is(fs.readFileSync(tempWrite.sync('unicorn'), 'utf8'), 'unicorn');
 });
