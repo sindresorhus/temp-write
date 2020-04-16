a3f7e210c3c1e00d8d2a9b5ec3bb835effe763ad
Sindre Sorhus <sindresorhus@gmail.com>
2020-04-17 02:02:47 +0800

    Meta tweaks

--- index.test-d.ts
+++ index.test-d.ts
@@ -4,8 +4,8 @@ import tempWrite = require('.');
 expectType<Promise<string>>(tempWrite('unicorn'));
 expectType<Promise<string>>(tempWrite('unicorn', 'pony.png'));
 expectType<Promise<string>>(tempWrite(process.stdin, 'pony.png'));
-expectType<Promise<string>>(tempWrite(new Buffer('pony'), 'pony.png'));
+expectType<Promise<string>>(tempWrite(Buffer.from('pony'), 'pony.png'));
 
 expectType<string>(tempWrite.sync('unicorn'));
-expectType<string>(tempWrite.sync(new Buffer('unicorn')));
+expectType<string>(tempWrite.sync(Buffer.from('unicorn')));
 expectType<string>(tempWrite.sync('unicorn', 'pony.png'));
